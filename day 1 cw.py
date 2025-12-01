1a1a84c4d9b7bfd5ca9b10ca623c7589794bff57
karthika.k.s <karthikasuresh31@gmail.com>
2025-12-01 15:24:30 +0530

    first commit

---
+++ day 1 cw.py
@@ -0,0 +1,25 @@
+applejuice = 15.5
+orangejuice = 20
+grapejuice = 10.25
+totalvolume = (applejuice+orangejuice+grapejuice)
+print(totalvolume)
+
+totalvolume = int(totalvolume)
+print(totalvolume)
+
+totalvolume = str(totalvolume)
+print("The string value is:"+totalvolume)
+
+import random
+bonusliters = (random.randrange(5,10))
+print(bonusliters)
+totalvolume = int(totalvolume)
+finaltotal = bonusliters+totalvolume
+print(finaltotal)
+
+
+
+
+
+
+
---
+++ day 1 hw.py
@@ -0,0 +1,44 @@
+#Use variables to store the prices and quantities.
+rice = 45
+sugar = 40
+oil = 130
+
+riceqty = 3
+sugarqty = 2.5
+oilqty = 1.8
+
+ricetotal = rice * riceqty
+sugartotal = sugar * sugarqty
+oiltotal = oil * oilqty
+
+print(ricetotal)
+print(sugartotal)
+print(oiltotal)
+
+#Use appropriate data types
+print(type(ricetotal))
+print(type(sugartotal))
+print(type(oiltotal))
+
+#Calculate and print the total price for each item and the final total bill.
+finalbill = ricetotal + sugartotal + oiltotal
+print(finalbill)
+
+#Show the total bill as an integer and also as a string.
+
+finalbill = str(finalbill)
+print(finalbill)
+
+finalbill = int(finalbill)
+print(finalbill)
+
+
+#Use random number generation to add a random ?5–?10 delivery charge
+deliverycharge = random.randrange(5,10)
+print(deliverycharge)
+
+#Show the final bill amount including delivery charge.
+finalamount = finalbill + deliverycharge
+print(finalamount)
+
+
